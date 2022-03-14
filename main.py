39f9bb677bbdbd3ff62c7d5fffe5ee4400cda05a
BuildTools <unconfigured@null.spigotmc.org>
2022-03-14 15:17:07 +0100

    init

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pygame
+
+pygame.init()
+fenetre = pygame.display.set_mode((640, 480), pygame.RESIZABLE)
+
+fond = pygame.image.load("./exemple/background.jpg").convert()
+continuer = 1
+while continuer:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            continuer = 0
+    fenetre.blit(fond, (0, 0))
+
+    pygame.display.flip()
+pygame.quit()
